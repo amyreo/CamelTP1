e68f5e917689b16bde176c4f9b228dad95a99871
jonathan <jonathan@BKE592H>
2022-12-15 17:08:37 +0100

    creation customer

---
+++ src/main/java/org/inti/Customer.java
@@ -0,0 +1,48 @@
+package org.inti;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.CamelContextAware;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.core.annotation.Order;
+
+public class Customer extends RouteBuilder implements InitializingBean, CamelContextAware {
+	private CamelContext camelContext;
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		if (camelContext == null) {
+			this.addRoutesToCamelContext(camelContext);
+
+		}
+
+	}
+
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		from("myTimer?fixedRate=true&period=60000")
+		.to("direct:cafe")
+		.process(new Processor() {
+			
+			@Override
+			public void process(Exchange exchange) throws Exception {
+				// TODO Auto-generated method stub
+				Order order = exchange.getIn().getBody(Order.class);
+			}
+		});
+
+	}
+
+	public CamelContext getCamelContext() {
+		return camelContext;
+	}
+
+	public void setCamelContext(CamelContext camelContext) {
+		this.camelContext = camelContext;
+	}
+
+}
