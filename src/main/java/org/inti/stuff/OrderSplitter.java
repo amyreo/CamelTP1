4adb69f534b83b9260c2985102a9618cd347ca89
Drozerd <debeneyf@gmail.com>
2022-12-16 10:12:57 +0100

    Aout classe OrderSplitter

---
+++ src/main/java/org/inti/stuff/OrderSplitter.java
@@ -0,0 +1,20 @@
+package org.inti.stuff;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.inti.OrderItem;
+import org.inti.model.Order;
+
+public class OrderSplitter {
+
+	
+	public List<OrderItem> split(Order order)
+	{	
+		List<OrderItem> lisOrderItems = new ArrayList<>();
+		for (OrderItem orderItem : lisOrderItems) {
+			return lisOrderItems;
+		}
+		return null;	
+	}
+}
