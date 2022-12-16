cb4d4a513ef98232850b5e2dcdbfa278dfa279d7
Drozerd <debeneyf@gmail.com>
2022-12-16 09:52:17 +0100

    Ajout class Barista

---
+++ src/main/java/org/inti/stuff/Barista.java
@@ -0,0 +1,31 @@
+package org.inti.stuff;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.apache.camel.model.DelayDefinition;
+import org.inti.OrderItem;
+import org.inti.model.Drink;
+
+public class Barista {
+	private static Drink drink;
+	private long hotDrinkDelay = 300;
+	private long coldDrinkDelay = 100;
+	private AtomicInteger hotDrinkCounter = new AtomicInteger();
+	private AtomicInteger coldDrinkCounter = new AtomicInteger();
+	
+	public Drink prepareHotDrink(OrderItem orderItem) throws InterruptedException 
+	{
+		Thread.sleep(hotDrinkDelay);
+		return drink;
+		
+	}
+
+	public Drink prepareColdDrink(OrderItem orderItem) throws InterruptedException 
+	{
+		Thread.sleep(coldDrinkDelay);
+		return drink;
+		
+	}
+
+	
+}
