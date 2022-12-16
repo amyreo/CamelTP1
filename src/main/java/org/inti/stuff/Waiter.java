58dd8f0c103976da6b81f59bba71fd6945fdb2c4
FlorentEstur <warcr@DESKTOP-3VUL9G4>
2022-12-16 10:18:48 +0100

    Ajout Waiter

---
+++ src/main/java/org/inti/stuff/Waiter.java
@@ -0,0 +1,26 @@
+package org.inti.stuff;
+
+import java.util.List;
+
+import org.inti.Delivery;
+import org.inti.model.Drink;
+
+public class Waiter {
+
+	private static int count = 0;
+	
+	public Delivery  prepareDelivery(List<Drink> listDrink) {
+		count ++;
+		Delivery delivery = new Delivery(listDrink, count);
+		return delivery;
+	}
+	
+	public void deliverCafes(Delivery delivery) {
+		System.out.println(delivery.toString());
+	}
+	
+	public int checkOrder(Drink drink) {
+		int orderNumber = drink.getOrderNumber();
+		return orderNumber;
+	}
+}
